35095ee1b45ac118cbe4fc1ece41a77708ad2be8
AgustinItuarte <agustin242536@gmail.com>
2023-01-01 23:12:57 -0300

    Diseño retocado

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import paginaPrincipal from "./principal";
+import menu from "./menu";
 import './style.css';
 
 paginaPrincipal();
+
+const btn_inicio = document.querySelector('.btn-inicio');
+const btn_menu = document.querySelector('.btn-menu');
+const btn_contacto = document.querySelector('.btn-contacto');
+let buttons = document.querySelectorAll('button');
+const pagina_principal = document.querySelector('.pagina-principal')
+
+for (let i = 0; i < buttons.length; i++) {
+
+    buttons[i].addEventListener('click', () => {
+        if (buttons[i].className === 'btn-inicio') {
+            pagina_principal.remove();
+            paginaPrincipal();
+            buttons = document.querySelectorAll('button');
+        } else if (buttons[i].className === 'btn-menu') {
+            pagina_principal.remove();
+            menu();
+            buttons = document.querySelectorAll('button');
+        }
+    })
+    
+}
--- src/principal.js
+++ src/principal.js
@@ -1,4 +1,5 @@
 import Icon from './vinos.jpg';
+import Vino from './glass-wine.png';
 
 export default function paginaPrincipal() {
 
@@ -11,13 +12,16 @@ export default function paginaPrincipal() {
     const header_tab2 = document.createElement('button');
     const header_tab3 = document.createElement('button');
     const cont_main = document.createElement('div');
-    const nombre = document.createElement('h1')
-    const bienvenida = document.createElement('div')
-    const horarios = document.createElement('div')
-    const localizacion = document.createElement('div')
-    const ul_footer = document.createElement('ul')
-    const li1_footer = document.createElement('li')
-    const li2_footer = document.createElement('li')
+    const nombre = document.createElement('h1');
+    const bienvenida = document.createElement('div');
+    const horarios = document.createElement('div');
+    const localizacion = document.createElement('div');
+    const ul_footer = document.createElement('ul');
+    const li1_footer = document.createElement('li');
+    const li2_footer = document.createElement('li');
+    const bienvenida_box = document.createElement('div');
+    const myIcon = new Image();
+    myIcon.src = Vino;
 
     contenedor.appendChild(pagina_principal);
     pagina_principal.appendChild(header);
@@ -27,13 +31,15 @@ export default function paginaPrincipal() {
     header.appendChild(header_tab2);
     header.appendChild(header_tab3);
     main_content.appendChild(cont_main);
-    cont_main.appendChild(nombre);
+    cont_main.appendChild(bienvenida_box);
     cont_main.appendChild(bienvenida);
     cont_main.appendChild(horarios);
     cont_main.appendChild(localizacion);
     footer.appendChild(ul_footer);
     ul_footer.appendChild(li1_footer);
     ul_footer.appendChild(li2_footer);
+    bienvenida_box.appendChild(nombre);
+    bienvenida_box.appendChild(myIcon);
     
     pagina_principal.classList.add('pagina-principal');
     header.classList.add('header');
@@ -46,11 +52,12 @@ export default function paginaPrincipal() {
     header_tab1.classList.add('btn-inicio');
     header_tab2.classList.add('btn-menu');
     header_tab3.classList.add('btn-contacto');
+    bienvenida_box.classList.add('bienvenida-box');
 
     header_tab1.textContent = 'Inicio';
     header_tab2.textContent = 'Menu';
     header_tab3.textContent = 'Contacto';
-    nombre.textContent = 'Bienvenidos';
+    nombre.innerHTML = 'Bienvenidos';
     bienvenida.innerHTML = '<h2>El Vino</h2> <br> El Restaurante El Vino nace a principios del año 2000 como un desafío de su propietario. Luego de haber estudiado y trabajado diez años fuera del país, aprendió que se puede lograr un buen servicio y mejor calidad sin tener que dañar el bolsillo del cliente.';
     horarios.innerHTML = '<h2>Horarios</h2> <br> Domingo: 8am - 8pm <br> Lunes: 6am - 6pm <br> Lunes: 6am - 6pm <br> Martes: 6am - 6pm <br> Miercoles: 6am - 10pm <br> Jueves: 6am - 10pm <br> Viernes: 6am - 10pm <br> Viernes: 6am - 10pm'
     localizacion.innerHTML = '<h2>Localizacion</h2> <br> Guipúzcoa 350, Montevideo 11300 Uruguay'
